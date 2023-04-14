018e3db03880ca34e9902482d6f5bf3bb651e117
Yonanda <yonandaharyono@gmail.com>
2023-04-14 17:08:56 +0700

    initial commit

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+// -------------------- ENVIRONMENT -------------------- //
+$alas = 10;
+$tinggi = 5;
+// -------------------- LUAS BANGUN DATAR -------------------- //
+// 1. Luas Segitiga
+$segitiga = 1/2 * $alas * $tinggi;
+// 2. Luas Persegi
+// 3. Luas Persegi Panjang
+// 4. Luas Jajar Genjang
+// 5. Luas Lingkaran
+// 6. Luas Belah Ketupat
+// -------------------- PRINT HASIL -------------------- //
+echo 'Hasil Segitiga adalah = ' . $segitiga;
